da6b88f0d86c4285d5abfd664a2d9d80e255e0b5
Yingzi Wang <112147790+7lu0420@users.noreply.github.com>
2022-12-13 10:05:10 -0800

    Add files via upload

--- sudoku/main.py
+++ sudoku/main.py
@@ -1,19 +1,20 @@
 from Sudoku_Solve import configs
 from Sudoku_Solve.Game_Sudoku import Game_Sudoku
+import pygame
  
  
-def main(args):
+def main(args_form,args_selectedform,args_block):
     """
     screen_width: Width of the form
     screen_height: Height of the form
     """
-    screen_width = args.screen_width
-    screen_height = args.screen_height
-    selected_width = args.selected_width
-    selected_height = args.selected_height
-    block_gap = args.block_gap
-    block_size = args.block_size
-    level = args.level
+    screen_width = args_form.screen_width
+    screen_height = args_form.screen_height
+    selected_width = args_selectedform.selected_width
+    selected_height = args_selectedform.selected_height
+    block_gap = args_block.block_gap
+    block_size = args_block.block_size
+    level = args_block.level
  
     game = Game_Sudoku(screen_width, screen_height, selected_width, selected_height,
                        block_gap, block_size, level)
@@ -21,5 +22,7 @@ def main(args):
     # game.Form()
 
 if __name__ == '__main__':
-    args = configs.parse_args()
-    main(args)
+    args_form = configs.parse_args1()
+    args_selectedform = configs.parse_args2()
+    args_block = configs.parse_args3()
+    main(args_form,args_selectedform,args_block)
