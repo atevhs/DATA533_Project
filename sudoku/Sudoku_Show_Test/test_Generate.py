1d9a9c6fda12c2f33f17a50bc16317f994c2e994
Shveta Sharma <62797751+atevhs@users.noreply.github.com>
2022-12-17 00:24:31 -0800

    Add files via upload

--- sudoku/Sudoku_Show_Test/test_Generate.py
+++ sudoku/Sudoku_Show_Test/test_Generate.py
@@ -4,11 +4,13 @@ import random
 import numpy as np
 
 class TestAdd(unittest.TestCase):
-        
-    def test_Las_sdk(self): # test routine
+    def setUp(self):
         self.martix = np.zeros((9, 9), dtype='i1')
         self.Nums = {1, 2, 3, 4, 5, 6, 7, 8, 9} 
         self.uniqueMartix = []
+        #self.possible_num = 30
+        
+    def test_Las_sdk(self): # test routine
         self.assertEqual(gd.Las_sdk(self,counts=30), True)
         self.assertEqual(gd.Las_sdk(self,counts=40), True)
         self.assertEqual(gd.Las_sdk(self,counts=50), True)
